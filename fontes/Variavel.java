8e198279211081ca13a1e5937c609d7a9c65eafe
cleitonPiccini <cleiton-piccini@hotmail.com>
2014-09-23 13:05:27 -0300

    Criação classe Variavel

---
+++ fontes/Variavel.java
@@ -0,0 +1,26 @@
+class Variavel{
+	
+	private string nome, tipo, 
+	private int valor;
+
+	public void getNome() {
+		return this.nome;
+	}
+
+	public void getTipo() {
+		return this.tipo;
+	}
+
+	public void getValor() {
+		return this.valor;
+	}
+
+	public string setNome(n){
+		this.nome = n;
+	}
+	
+	public string setTipo(t) {
+		this.tipo = t;
+	}
+		
+}
